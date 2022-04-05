04358d7a81d711ff4d9e4549989228c0ede4b6dd
im4kshay <akshaysayre7@gmail.com>
2022-04-06 01:02:41 +0530

    Ability to compute Employee Wage for multiple companies

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -11,13 +11,16 @@ namespace EmployeeWageComputation
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Empolyee Wage Computation Program\n");
-            EmpAttendance.GetEmpAttendance(); 
+            EmpAttendance.GetEmpAttendance();
             EmployeeWage.GetEmpWage();
             PartTimeEmployee.PartTimeWage();
             SwitchCase.DailyEmpWage();
             MonthWages.MonthlyEmpWage();
             WageTillCondition.GetWageTillCondition();
             RefactorWithClassMethod.RefactorGetMonthlyEmpWage();
+            EmpWageForMultipleCompanies.ComputeEmpWage("Google", 20, 2, 10);
+            EmpWageForMultipleCompanies.ComputeEmpWage("Microsoft", 10, 4, 20);
+            EmpWageForMultipleCompanies.ComputeEmpWage("Deloitte", 15, 3, 15);
             Console.ReadLine();
         }
     }
