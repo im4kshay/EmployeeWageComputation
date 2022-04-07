62ae0a0c80f4f680899ecc00a3e504c9c0d4cff1
im4kshay <akshaysayre7@gmail.com>
2022-04-08 02:50:56 +0530

    Store the Daily Wage along with the Total Wage

--- EmployeeWageComputation/CompanyEmpWage.cs
+++ EmployeeWageComputation/CompanyEmpWage.cs
@@ -9,27 +9,34 @@ namespace EmployeeWageComputation
     class CompanyEmpWage
     {
         public string company;
-        public int empRatePerHour;
-        public int numOfWorkingDays;
-        public int maxHoursPerMonth;
-        public int totalEmpWage;
+        public int emp_Wage_Per_Hr;
+        public int working_Days_Per_Month;
+        public int max_Hrs_Per_Month;
+        public int total_Emp_Wage;
+        public int dailyEmpWage;
 
-        public CompanyEmpWage(string company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth)
+        public CompanyEmpWage(string company, int emp_Wage_Per_Hr, int working_Days_Per_Month, int max_Hrs_Per_Month)
         {
             this.company = company;
-            this.empRatePerHour = empRatePerHour;
-            this.numOfWorkingDays = numOfWorkingDays;
-            this.maxHoursPerMonth = maxHoursPerMonth;
+            this.emp_Wage_Per_Hr = emp_Wage_Per_Hr;
+            this.working_Days_Per_Month = working_Days_Per_Month;
+            this.max_Hrs_Per_Month = max_Hrs_Per_Month;
         }
 
-        public void setTotalEmpWage(int totalEmpWage)
+        public CompanyEmpWage(string company, int daily_Emp_Wage, int total_Emp_Wage)
         {
-            this.totalEmpWage = totalEmpWage;
+            this.company = company;
+            this.dailyEmpWage = daily_Emp_Wage;
+            this.total_Emp_Wage = total_Emp_Wage;
         }
 
-        public string toString()
+        public void SetTotalEmpWage(int totalEmpWage)
         {
-            return "Total Emp Wage for Company : " + this.company + " is :" + this.totalEmpWage + "\n";
+            this.total_Emp_Wage = totalEmpWage;
+        }
+        public override string ToString()
+        {
+            return "Total Employee Wage for company: " + company + " is: " + total_Emp_Wage + "\n";
         }
     }
 }
