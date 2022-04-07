d2b4c4380877b5493a6af292a906589f65986f36
im4kshay <akshaysayre7@gmail.com>
2022-04-07 22:18:56 +0530

    Ability to manage Employee Wage of multiple companies using Interface approach

--- EmployeeWageComputation/EmpWageBuilderArray.cs
+++ EmployeeWageComputation/EmpWageBuilderArray.cs
@@ -8,47 +8,68 @@ namespace EmployeeWageComputation
 {
     class EmpWageBuilderArray
     {
-        public const int EMP_FULLTIME = 1, EMP_PARTTIME = 2;
-        private int noOfCompany = 0;
-
+        //constants
+        public const int EMF_FULL_TIME = 1;
+        public const int EMF_PART_TIME = 2;
+        private int numOfCompany = 0;
         private CompanyEmpWage[] companyEmpWageArray;
-        public static Random random = new Random();
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="EmpWageBuilderArray"/> class.
+        /// </summary>
         public EmpWageBuilderArray()
         {
             this.companyEmpWageArray = new CompanyEmpWage[5];
         }
 
+        /// <summary>
+        /// Adds the company emp wage.
+        /// </summary>
+        /// <param name="company">The company.</param>
+        /// <param name="empRatePerHour">The emp rate per hour.</param>
+        /// <param name="numOfWorkingDays">The number of working days.</param>
+        /// <param name="maxHoursPerMonth">The maximum hours per month.</param>
         public void AddCompanyEmpWage(string company, int emp_Wage_Per_Hr, int working_Days_Per_Month, int max_Hrs_Per_Month)
         {
-            companyEmpWageArray[this.noOfCompany] = new CompanyEmpWage(company, emp_Wage_Per_Hr, working_Days_Per_Month, max_Hrs_Per_Month);
-            noOfCompany++;
+            companyEmpWageArray[this.numOfCompany] = new CompanyEmpWage(company, emp_Wage_Per_Hr, working_Days_Per_Month, max_Hrs_Per_Month);
+            numOfCompany++;
         }
 
+        /// <summary>
+        /// Computes the emp wage.
+        /// </summary>
         public void ComputeEmpWage()
         {
-            for (int i = 0; i < noOfCompany; i++)
+            for (int i = 0; i < numOfCompany; i++)
             {
-                companyEmpWageArray[i].SetTotalEmpWage(this.ComputeEmpWage(this.companyEmpWageArray[i]));
-                Console.WriteLine(companyEmpWageArray[i].ToString());
+                companyEmpWageArray[i].setTotalEmpWage(this.ComputeEmpWage(this.companyEmpWageArray[i]));
+                Console.WriteLine(this.companyEmpWageArray[i].toString());
             }
         }
 
-        private int ComputeEmpWage(CompanyEmpWage companyEmpWage)
+        /// <summary>
+        /// Computes the emp wage.
+        /// </summary>
+        /// <param name="companyEmpWage">The company emp wage.</param>
+        /// <returns></returns>
+        public int ComputeEmpWage(CompanyEmpWage companyEmpWage)
         {
-            int empHrs = 0, total_Emp_Hrs = 0, totalWorkingDays = 1, daily_Emp_Wage = 0;
-
-            while (totalWorkingDays <= companyEmpWage.working_Days_Per_Month && total_Emp_Hrs <= companyEmpWage.max_Hrs_Per_Month)
+            int empHrs = 0;
+            int total_Emp_Hrs = 0;
+            int totalWorkingDays = 0;
+            int daily_Emp_Wage = 0;
+            while (total_Emp_Hrs <= companyEmpWage.max_Hrs_Per_Month && totalWorkingDays < companyEmpWage.working_Days_Per_Month)
             {
-                int randomInput = random.Next(0, 3);
-
-                switch (randomInput)
+                totalWorkingDays++;
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
                 {
-                    case EMP_FULLTIME:
-                        empHrs = 8;
-                        break;
-                    case EMP_PARTTIME:
+                    case EMF_PART_TIME:
                         empHrs = 4;
+                        break;
+                    case EMF_FULL_TIME:
+                        empHrs = 8;
                         break;
                     default:
                         empHrs = 0;
@@ -59,14 +80,9 @@ namespace EmployeeWageComputation
                 companyEmpWage.total_Emp_Wage += daily_Emp_Wage;
                 total_Emp_Hrs += empHrs;
                 totalWorkingDays++;
-
-
             }
-            Console.WriteLine("Total Days: {0}, Total working hours: {1}", (totalWorkingDays - 1), total_Emp_Hrs);
-            //Console.WriteLine("Total Employee Wage for company " + company + " is: " + total_Emp_Wage + "\n");
-            return companyEmpWage.total_Emp_Wage;
+            return total_Emp_Hrs * companyEmpWage.emp_Wage_Per_Hr;
         }
-
 
     }
 }
